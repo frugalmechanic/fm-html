c0b1d1d09e71f3ed0d820a706a177007a2b0e4e1
Tim Underwood <timunderwood@gmail.com>
2021-02-24 14:29:11 -0800

    Scalatest 3.2 API Updates

--- src/test/scala/fm/html/TestHtml5.scala
+++ src/test/scala/fm/html/TestHtml5.scala
@@ -15,31 +15,32 @@
  */
 package fm.html
 
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-final class TestHtml5 extends FunSuite with Matchers {
+final class TestHtml5 extends AnyFunSuite with Matchers {
   test("Basic Attribute + Body Test") {
-    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "foo") { "bar" } } should equal("""<div id="foo">bar</div>""")
+    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "foo") { "bar" } } shouldBe """<div id="foo">bar</div>"""
   }
 
   test("Attributes") {
-    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "foo", cls="class", title="Foo & Bar", attrs = Map("non-standard" -> "non & standard"), data = Map("foo" -> "foo & data", "bar" -> "bar & data")) { "bar" } } should equal("""<div class="class" id="foo" title="Foo &amp; Bar" data-foo="foo &amp; data" data-bar="bar &amp; data" non-standard="non &amp; standard">bar</div>""")
+    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "foo", cls="class", title="Foo & Bar", attrs = Map("non-standard" -> "non & standard"), data = Map("foo" -> "foo & data", "bar" -> "bar & data")) { "bar" } } shouldBe """<div class="class" id="foo" title="Foo &amp; Bar" data-foo="foo &amp; data" data-bar="bar &amp; data" non-standard="non &amp; standard">bar</div>"""
   }
 
   test("Body Test") {
-    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV() { "bar" } } should equal("""<div>bar</div>""")
+    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV() { "bar" } } shouldBe """<div>bar</div>"""
   }
 
   test("No Body Test") {
-    Html5StringBuilder.captureConcise{ implicit ctx => Html5.br() } should equal("""<br>""")
+    Html5StringBuilder.captureConcise{ implicit ctx => Html5.br() } shouldBe """<br>"""
   }
 
   test("Include Blank Attribute Values") {
-    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "") { "bar" } } should equal("""<div id="">bar</div>""")
-    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "  ") { "bar" } } should equal("""<div id="  ">bar</div>""")
+    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "") { "bar" } } shouldBe """<div id="">bar</div>"""
+    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = "  ") { "bar" } } shouldBe """<div id="  ">bar</div>"""
   }
 
   test("Ignore Null Attribute Values") {
-    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = null) { "bar" } } should equal("""<div>bar</div>""")
+    Html5StringBuilder.captureConcise{ implicit ctx => Html5.DIV(id = null) { "bar" } } shouldBe """<div>bar</div>"""
   }
 }
